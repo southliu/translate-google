2d42beba8364d643009f9aec7a3a143c060de383
southliu <1275093225@qq.com>
2023-02-25 21:56:13 +0800

    style: 基础样式

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,13 +1,47 @@
 import { useState } from 'react'
+import './popup.css'
+
+const tabs = [
+  { label: '英语', key: 'eng' },
+  { label: '中文(简体)', key: 'zh' }
+]
 
 function IndexPopup() {
-  const [data, setData] = useState('')
+  const [active, setActive] = useState('eng') // 当前选中
+
+  /** 关闭 */
+  const onClose = () => {
+    // chrome
+    console.log('关闭')
+  }
 
   return (
-    <div className='flex w-300px'>
-      <div>英语</div>
-      <div>中文(简体)</div>
-    </div>
+    <>
+      <div className='header'>
+        <div className='translate-box'>
+          {
+            tabs.map(item => (
+              <div
+                className={`item ${active === item.key ? 'active' : ''}`}
+                key={item.key}
+                onClick={() => setActive(item.key)}
+              >
+                { item.label }
+              </div>
+            ))
+          }
+        </div>
+
+        <div
+          className='close'
+          onClick={onClose}
+        />
+      </div>
+
+      <div className='footer'>
+        South 翻译
+      </div>
+    </>
   )
 }
 
